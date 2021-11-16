1fa485d70260eed91b550a3061193b4bf98bbb9c
NishNilanka <nishan.nilanka@gmail.com>
2021-11-16 23:58:25 +0530

    Bug fixes

--- static/javascript/day.js
+++ static/javascript/day.js
@@ -1,23 +1,4 @@
 
-const button = document.getElementById('submit_btn');
-function onload_func()
-{
-    document.getElementById("plot").style.display = "none";
-}
 
 
-button.addEventListener('click', async _ => {
-  try {
-    const response = await fetch('/selectDate', {
-      method: 'post',
-      body: {
-        let startDate =  document.getElementById('submit_btn');
-      }
-    });
-    console.log('Completed!', response);
-  } catch(err) {
-    console.error(`Error: ${err}`);
-  }
-   document.getElementById("plot").style.display = "block";
-});
 
